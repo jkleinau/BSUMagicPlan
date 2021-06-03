0a8fc2aebfb9aff2d65de32f6911996923b65385
Justus kleinau <justus@kleinau-online.de>
2021-06-03 13:50:40 +0200

    Buttons disabled

--- models/plan.js
+++ models/plan.js
@@ -1,51 +1,55 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const plan = new mongoose.Schema({
-    id: {
-      type: String,
-      required:true,
-      unique:true
-    },
-    name: {
-      type: String,
-      required:true
-    },
-    address: {
-      street: {
-        type: String
-      },
-      street_number: {
-        type: Number
-      },
-      postal_code: {
-        type: String
-      },
-      city: {
-        type: String
-      },
-      country: {
-        type: String
-      },
-      longitude: {
-        type: Number
-      },
-      latitude: {
-        type: Number
-      }
-    },
-    creation_date: {
-      type: Date,
-      required:true
-    },
-    update_date: {
-      type: Date,
-      required:true
-    },
-    thumbnail_url: {
-      type: String
-    },
-    public_url: {
-      type: String
-    }
-  })
+	id: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	name: {
+		type: String,
+		required: true,
+	},
+	address: {
+		street: {
+			type: String,
+		},
+		street_number: {
+			type: String,
+		},
+		postal_code: {
+			type: String,
+		},
+		city: {
+			type: String,
+		},
+		country: {
+			type: String,
+		},
+		longitude: {
+			type: Number,
+		},
+		latitude: {
+			type: Number,
+		},
+	},
+	creation_date: {
+		type: Date,
+		required: true,
+	},
+	update_date: {
+		type: Date,
+		required: true,
+	},
+	thumbnail_url: {
+		type: String,
+	},
+	public_url: {
+		type: String,
+	},
+	xml_file: {
+		type: Boolean,
+		default: false,
+	},
+});
 
-  module.exports = mongoose.model('Plan', plan)
+module.exports = mongoose.model('Plan', plan);
--- routes/index.js
+++ routes/index.js
@@ -1,94 +1,90 @@
-const { response } = require('express')
-const express = require('express')
-const router = express.Router()
-const request = require('request')
-const Plan = require('../models/plan')
+const { response } = require('express');
+const express = require('express');
+const router = express.Router();
+const request = require('request');
+const Plan = require('../models/plan');
 
-let options={
-    url:'https://cloud.magic-plan.com/api/v2/workgroups/plans?sort=Plans.update_date&direction=desc&page=1',
-    headers:{
-        accecpt: 'application/json',
-        key: 'dd0c7925689fbf4f2083412497c30f9d2445',
-        customer: '5c7ef0e0b4132'  
-    }
-    
-}
+let options = {
+	url: 'https://cloud.magic-plan.com/api/v2/workgroups/plans?sort=Plans.update_date&direction=desc&page=2',
+	headers: {
+		accecpt: 'application/json',
+		key: 'dd0c7925689fbf4f2083412497c30f9d2445',
+		customer: '5c7ef0e0b4132',
+	},
+};
 
-router.get('/', async(req,res)=>{
-    let query = Plan.find()
-    if(req.query.searchTerm != null && req.query.searchTerm != ''){
-        query = query.regex('name', new RegExp(req.query.searchTerm, 'i'))
-    }
-    req.plans = await query.limit(300).exec()
-    res.render('index',{
-        plans: req.plans,
-        searchTerm: req.query.searchTerm
-    })
-})
+router.get('/', async (req, res) => {
+	let query = Plan.find();
+	if (req.query.searchTerm != null && req.query.searchTerm != '') {
+		query = query.regex('name', new RegExp(req.query.searchTerm, 'i'));
+	}
+	req.plans = await query.limit(300).exec();
+	res.render('index', {
+		plans: req.plans,
+		searchTerm: req.query.searchTerm,
+	});
+});
 
-router.get('/refresh', async (req, res) =>{
+router.get('/refresh', async (req, res) => {
+	request.get(options, async (err, response, body) => {
+		if (err) return;
+		const info = JSON.parse(body);
 
-    request.get(options, async (err, response, body)=>{
-        if(err) return
-        const info = JSON.parse(body)
+		for (const plan of info.data.plans) {
+			const newPlan = new Plan({
+				id: plan.id,
+				name: plan.name,
+				address: {
+					street: plan.address.street,
+					street_number: plan.address.street_number,
+					postal_code: plan.address.postal_code,
+					city: plan.address.city,
+					country: plan.address.country,
+					longitude: plan.address.longitude,
+					latitude: plan.address.latitude,
+				},
+				creation_date: plan.creation_date,
+				update_date: plan.update_date,
+				thumbnail_url: plan.thumbnail_url,
+				public_url: plan.public_url,
+			});
 
-        for (const plan of info.data.plans) {
-            const newPlan = new Plan({
-                id: plan.id,
-                name: plan.name,
-                address : {
-                    street:  plan.address.street,
-                    street_number:  plan.address.street_number,
-                    postal_code: plan.address.postal_code,
-                    city:  plan.address.city,
-                    country:  plan.address.country,
-                    longitude:  plan.address.longitude,
-                    latitude:  plan.address.latitude,
-                },
-                creation_date: plan.creation_date,
-                update_date: plan.update_date,
-                thumbnail_url: plan.thumbnail_url,
-                public_url: plan.public_url
-            })
+			const existingPlan = await Plan.find({ id: plan.id }).exec();
+			console.log('🚀 ~ request.get ~ existingPlan', existingPlan);
+			// If the Plan does not exist create a new one and save it
+			if (existingPlan.length == 0) {
+				try {
+					const newP = await newPlan.save();
+					console.log('Creating new Plan');
+					console.log(newP);
+				} catch (e) {
+					console.error(e);
+				}
+			} else {
+				// If the Plan already exists but information changed
+				if (
+					new Date(plan.update_date).getTime() >
+					new Date(existingPlan.update_date).getTime()
+				) {
+					existingPlan = newPlan;
+					try {
+						const updatedPlan = await existingPlan.save();
+						console.log('Updated Plan');
+						console.log(updatedPlan);
+					} catch (e) {
+						console.error(e);
+					}
+				} else {
+					// If the Plan exists and is up to date
+					console.log('Terminating at Plan');
+					console.log(plan);
+					break;
+				}
+			}
+		}
+	});
 
-            const existingPlan = await Plan.find({id:plan.id}).exec()
-            // If the Plan does not exist create a new one and save it
-            if(existingPlan === null){
-                try {
-                    const newP = await newPlan.save()
-                    console.log("Creating new Plan")
-                    console.log(newP)
-                } catch (e) {
-                    console.error(e)
-                }
-            }else{
-                // If the Plan already exists but information changed
-                if(new Date(plan.update_date).getTime() > new Date(existingPlan.update_date).getTime()){
-                    existingPlan = newPlan
-                    try {
-                        const updatedPlan = await existingPlan.save()
-                        console.log("Updated Plan")
-                        console.log(updatedPlan)
-                    } catch (e) {
-                        console.error(e)
-                    }
-                }else{
-                    // If the Plan exists and is up to date
-                    console.log("Terminating at Plan")
-                    console.log(plan)
+	res.redirect('/');
+});
 
-                    break
-                }
-            }
-            
-        }
-
-    })  
-    
-    
-    res.redirect('/')
-})
-
-
-
-module.exports = router
+module.exports = router;
